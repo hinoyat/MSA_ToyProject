0525b526aad6638613d7e847e6a037e745289322
Hinoyat <hhwj2270@gmail.com>
2025-04-18 21:49:42 +0900

    [BE] Feat: 기본 test-service 추가

---
+++ test-service-a/src/main/java/com/s206/testa/TestServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.s206.testa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServiceAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestServiceAApplication.class, args);
+    }
+
+}
---
+++ test-service-a/src/test/java/com/s206/testa/TestServiceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s206.testa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestServiceAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
